ca8dcc961bd177b77e20072c003b52cd26ba66bf
Brian Teachman <me@briant.me>
2012-09-22 16:49:21 -0700

    Updated gitignore to reflect previous changes.

---
+++ classes/Localhost/View.php
@@ -0,0 +1,213 @@
+<?php
+namespace Localhost;
+
+/**
+ * View
+ */
+class View
+{
+    /**
+     * @var array
+     */
+    private $config;
+    
+    public $title;
+
+    public $view = '';
+
+    private $content = '';
+
+    public function __construct ($title, $config=null)
+    {
+        $this->title = $title;
+        if (null !== $config) {
+            $this->config = $config;
+        }
+    }
+
+    public function __toString()
+    {
+        return (string) $this->view;
+    }
+    
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Return HTML header as string
+     *
+     * @param  string $title
+     * @return string
+     */
+    public function head($title)
+    {
+        $bootstrap = $this->config['assets'] . 'bootstrap/css/bootstrap.css';
+        $styles = $this->config['assets'] . 'styles.css';
+        $phpinfo = $this->config['scripts'] . 'phpinfo.php';
+
+        header('Content-type: text/html; charset=utf-8');
+
+        return <<<HEADER
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>localhost &raquo; {$title}</title>
+        <link href="{$bootstrap}" rel="stylesheet">
+        <link rel="stylesheet" href="{$styles}" />
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="/">{$title}</a>
+                    <ul class="nav">
+                        <li><a href="http://localhost/phpmyadmin/" target="blank">phpMyAdmin</a></li>
+                        <li><a href="{$phpinfo}" target="blank">phpinfo()</a></li>
+                        <li><a href="/?list=all">Docs</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <section class="lister row">
+HEADER;
+    }
+
+    /**
+     * Return HTML footer as string
+     *
+     * @return string
+     */
+    public function foot()
+    {
+        $bootstrap = $this->config['assets'] . 'bootstrap/js/bootstrap.js';
+
+        $footer = <<<FOOTER
+        </section>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <!-- Bootstrap plugins -->
+        <script src="{$bootstrap}"></script>
+
+        <script type="text/javascript">
+
+        </script>
+    </body>
+</html>
+FOOTER;
+
+        return $footer;
+    }
+
+    public function appendContent($content)
+    {
+        $this->content .= $content;
+    }
+
+    /**
+     * @param  array  $files
+     * @param  array  $0ptions
+     * @param  string $li_class_override
+     * @return string
+     */
+    public function titledListBlock($files, $options=null, $li_class_override=null)
+    {
+        $html = '<div id="' . $options["id"] . '" class="listing span4">'
+                . '<h2><i class="icon-folder-open"></i>' . ucfirst($options["title"]) . '</h2>'
+                . '<p>' . $options["tagline"] . "</p>\n"
+                . "<ul>\n";
+
+        foreach ($files as $listing) {
+
+            if ($li_class_override !== null) {
+                $listing["type"] = $li_class_override;
+            }
+            $html .= '<li class="' . $listing["type"] . '">'
+                     . '<a href="' . $options["slug"] . $listing["name"] . '">'
+                        . '<span class="link">' . $listing["name"] . '</span>'
+                     . '</a>'
+                   . "</li>\n";
+        }
+        $html .= "</ul>\n</div>\n";
+
+        $this->appendContent($html);
+    }
+
+    /**
+     * Either sets a message or shows it (depends if message is set)
+     *
+     * @param  [string     $message]
+     * @param  [int        $weight]
+     * @return bool|string
+     */
+    public function messenger($message=null, $weight=0)
+    {
+        $levels = array(
+            'info',
+            'notice',
+            'warning',
+            'error',
+        );
+
+        $tbicon = array(
+            'icon-info-sign',
+            'icon-question-sign',
+            'icon-exclamation-sign',
+            'icon-remove-sign',
+        );
+
+        if ($message !== null) {
+            $_SESSION["messenger"]["message"] = $message;
+
+            if (array_key_exists($weight, $levels)) {
+                $class = $levels[$weight];
+            } else {
+                $class = $levels[3];
+            }
+            $_SESSION["messenger"]["level"] = $class;
+
+            return true;
+        }
+        if (isset($_SESSION["messenger"])) {
+
+            $class = $_SESSION["messenger"]["level"];
+            $key = array_search($class, $levels);
+            $icon = $tbicon[$key];
+            $message = $_SESSION["messenger"]["message"];
+            unset($_SESSION["messenger"]);
+
+            $messenger = '<span class="' . $class . '">'
+                 . '<i class="' . $icon . '"></i>'
+                 . $message
+                 . '</span>';
+
+            return $messenger;
+        }
+
+        return false;
+    }
+
+    /**
+     * If templates, load; if title, set; then output content.
+     *
+     * @param string
+     */
+    public function render($content, $template=true, $title=null)
+    {
+        if ($template) {
+
+            if ($title === null) {
+                $title = $this->title;
+            }
+            $this->view = $this->head($title)
+                        . $this->messenger()
+                        . $this->content
+                        . $this->foot();
+        }
+        echo $this->view;
+    }
+}
---
+++ runtime.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Simple localhost interface.
+ * 
+ * @author Brian Teachman, Teachman Web Development <me@briant.me>
+ * @license WTFPL <http://sam.zoy.org/wtfpl/COPYING>
+ */
+session_start();
+ 
+// PSR-0 Loaders
+include 'vendor/SplClassLoader.php';
+$loader = new SplClassLoader('Localhost', realpath(__DIR__) . '/classes');
+$loader->register();
+$loader = new SplClassLoader('dflydev\markdown', realpath(__DIR__) . '/vendor/dflydev/markdown/src');
+$loader->register();
+
+$config = include 'runtime.config.php';
+
+$host = new \Localhost\ResourceLister(
+    new \Localhost\View('Teachman Web'),
+    $config
+);
+
+if (isset($_GET["list"])) {
+
+    if ($_GET["list"] == 'all') {
+        
+        $local_docs = $config['httpd'] . '/docs/';
+        $file_meta = array(
+            'title' => 'Local Docs',
+            'tagline' => 'Listing: localhost/docs',
+            'slug' => '?list=' . $local_docs,
+        );
+        $host->set($local_docs, $file_meta);
+
+        $pear_meta = array(
+            'title' => 'PEAR Docs',
+            'tagline' => 'Listing: ' . $config['pear_docs'],
+            'slug' => '?list=' . $config['pear_docs'],
+        );
+        $host->set($config['pear_docs'], $pear_meta);
+        
+        $host->view();
+        
+    } else {
+
+        $style = $host->set($_GET["list"]);
+        $host->view($style);
+        
+    }
+    
+} else {
+
+    $file_meta = array(
+        'title' => 'localhost',
+        'tagline' => 'Listing: ' . getcwd(),
+//         'slug' => '?list=' . $config['httpd'] . '/',
+    );
+    $host->set($config['current_dir'], $file_meta);
+
+    $vhost_meta = array(
+        'title' => 'VirtualHost',
+        'tagline' => 'Site Development',
+        'slug' => 'http://',
+    );
+    $host->set('/etc/apache2/sites-enabled', $vhost_meta, 'vhost');
+    
+    $host->view();
+}
+?>
